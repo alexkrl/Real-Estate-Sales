afd9bf5768ca3e6c4702456da8b362fd85c39f3e
Alex Korolov <alexander.korolov@gmail.com>
2020-05-12 14:52:12 +0300

    rename sales -> estates

--- app/src/main/java/com/alexk/nadlansales/ui/estatesdata/EstatesDataFragment.kt
+++ app/src/main/java/com/alexk/nadlansales/ui/estatesdata/EstatesDataFragment.kt
@@ -2,14 +2,13 @@ package com.alexk.nadlansales.ui.estatesdata
 
 import android.os.Bundle
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.RecyclerView
 import com.alexk.nadlansales.R
 import com.alexk.nadlansales.ui.BaseFragment
-import kotlinx.android.synthetic.main.sales_data_fragment.*
+import kotlinx.android.synthetic.main.estates_data_fragment.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 
-class EstatesDataFragment : BaseFragment(R.layout.sales_data_fragment) {
+class EstatesDataFragment : BaseFragment(R.layout.estates_data_fragment) {
 
     private val viewModel: EstatesDataViewModel by viewModel()
     private var estatesHistoryDataAdapter: EstatesHistoryDataAdapter? = null
